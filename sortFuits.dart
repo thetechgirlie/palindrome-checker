562ea29eefaece8672f7e9d9789f79800668fb31
thetechgirlie <ogochukwu.nnaemezie.247214@unn.edu.ng>
2024-11-17 17:34:41 +0100

    Initial commit for basic Dart collection operation

---
+++ sortFuits.dart
@@ -0,0 +1,7 @@
+// favorite_fruits.dart
+// This program defines and sorts a list of favorite fruits alphabetically.
+void main(List<String> args) {
+  final myFavFruits = ['Banana', 'Apple', 'Orange', 'Watermelon', 'Cashew','Mango','Strawberry'];
+  myFavFruits.sort();
+  print(myFavFruits);
+}
