12b50ebf175b40a37640c1f14b311bc18d52fe9e
thetechgirlie <ogochukwu.nnaemezie.247214@unn.edu.ng>
2024-11-25 21:56:37 +0100

    Remove unwanted files

--- sortFuits.dart
+++
@@ -1,7 +0,0 @@
-// favorite_fruits.dart
-// This program defines and sorts a list of favorite fruits alphabetically.
-void main(List<String> args) {
-  final myFavFruits = ['Banana', 'Apple', 'Orange', 'Watermelon', 'Cashew','Mango','Strawberry'];
-  myFavFruits.sort();
-  print(myFavFruits);
-}
